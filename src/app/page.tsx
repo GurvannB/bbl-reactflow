66ae8ff66e01a8f9c811ad43339cad4b148824a9
Gurvann BRENNE <gurvann.brenne@gmail.com>
2026-04-13 14:21:44 +0200

    ➕ added reactflow with base component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+'use client';
+import {Background, ReactFlow} from "reactflow";
+import '@xyflow/react/dist/style.css';
+
 export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+    return (
+        <main className="w-full h-full">
+            <ReactFlow
+                nodes={[]}
+                edges={[]}
+            >
+                <Background/>
+            </ReactFlow>
+        </main>
+    );
 }

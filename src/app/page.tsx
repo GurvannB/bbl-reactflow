ed75a16304daa09642861c8ca90721adf0f68adb
Gurvann BRENNE <gurvann.brenne@gmail.com>
2026-04-13 16:58:18 +0200

    :sparkles: added edges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 'use client';
-import {applyNodeChanges, Background, Node, NodeChange, ReactFlow} from "reactflow";
+import {
+    addEdge,
+    applyEdgeChanges,
+    applyNodeChanges,
+    Background,
+    Connection,
+    Edge,
+    EdgeChange,
+    Node,
+    NodeChange,
+    ReactFlow
+} from "reactflow";
 import '@xyflow/react/dist/style.css';
 import {useCallback, useState} from "react";
 
@@ -7,23 +18,38 @@ const defaultNodes: Node[] = [
     {
         id: '1',
         data: {label: 'Input Node'},
-        position: {x: 250, y: 0},
-    }
+        position: {x: 250, y: 250},
+    },
+    {
+        id: '2',
+        data: {label: 'Input Node'},
+        position: {x: 250, y: 400},
+    },
 ];
+
+const defaultEdges: Edge[] = [];
 
 export default function Home() {
     const [nodes, setNodes] = useState<Node[]>(defaultNodes);
+    const [edges, setEdges] = useState<Edge[]>(defaultEdges);
 
     const onNodesChange = useCallback((changes: NodeChange[]) => setNodes((prev) => applyNodeChanges(changes, prev)), []);
+    const onEdgesChange = useCallback((changes: EdgeChange[]) => setEdges((prev) => applyEdgeChanges(changes, prev)), []);
+    const onConnect = useCallback((params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)), []);
 
     return (
         <main className="w-full h-full">
             <ReactFlow
                 nodes={nodes}
-                edges={[]}
+                edges={edges}
                 onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
                 snapGrid={[10, 10]}
                 snapToGrid={true}
+                defaultEdgeOptions={{
+                    type: "smoothstep",
+                }}
             >
                 <Background/>
             </ReactFlow>

02a0dc967bbe35e5e10cb6d5430cefce50fa5a3e
Gurvann BRENNE <gurvann.brenne@gmail.com>
2026-04-13 14:30:49 +0200

    ✨ added node changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 'use client';
-import {Background, ReactFlow} from "reactflow";
+import {applyNodeChanges, Background, Node, NodeChange, ReactFlow} from "reactflow";
 import '@xyflow/react/dist/style.css';
+import {useCallback, useState} from "react";
+
+const defaultNodes: Node[] = [
+    {
+        id: '1',
+        data: {label: 'Input Node'},
+        position: {x: 250, y: 0},
+    }
+];
 
 export default function Home() {
+    const [nodes, setNodes] = useState<Node[]>(defaultNodes);
+
+    const onNodesChange = useCallback((changes: NodeChange[]) => setNodes((prev) => applyNodeChanges(changes, prev)), []);
+
     return (
         <main className="w-full h-full">
             <ReactFlow
-                nodes={[]}
+                nodes={nodes}
                 edges={[]}
+                onNodesChange={onNodesChange}
+                snapToGrid={true}
+                snapGrid={[10, 10]}
             >
                 <Background/>
             </ReactFlow>

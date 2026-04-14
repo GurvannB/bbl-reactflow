46ef0feb05036960941518e64933c4fb8f299fda
Gurvann BRENNE <gurvann.brenne@gmail.com>
2026-04-14 10:55:50 +0200

    :sparkles: added resize

--- src/components/bunny-node.tsx
+++ src/components/bunny-node.tsx
@@ -1,10 +1,11 @@
-import {NodeProps} from "reactflow";
+import {NodeProps, NodeResizer} from "reactflow";
 import {BunnyData} from "@/lib/types";
 import {Rabbit} from "lucide-react";
 import {clsx} from "clsx";
 
 export default function BunnyNode({data, selected}: NodeProps<BunnyData>) {
-    return <div className={clsx("border p-2 rounded-lg bg-white flex gap-2", selected && "border-black")}>
+    return <div className={clsx("w-full h-full border p-2 rounded-lg bg-white flex gap-2 items-center justify-center", selected && "border-black")}>
         <Rabbit/> <p className="bold">{data.name}</p>
+        {selected && <NodeResizer/>}
     </div>
 }

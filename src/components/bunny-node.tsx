b94a9ac34524c4a389910668573d4542fad1bad9
Gurvann BRENNE <gurvann.brenne@gmail.com>
2026-04-14 10:54:35 +0200

    :sparkles: added selected state

--- src/components/bunny-node.tsx
+++ src/components/bunny-node.tsx
@@ -1,9 +1,10 @@
 import {NodeProps} from "reactflow";
 import {BunnyData} from "@/lib/types";
 import {Rabbit} from "lucide-react";
+import {clsx} from "clsx";
 
-export default function BunnyNode({data}: NodeProps<BunnyData>) {
-    return <div className="border p-2 rounded-lg bg-white flex gap-2">
+export default function BunnyNode({data, selected}: NodeProps<BunnyData>) {
+    return <div className={clsx("border p-2 rounded-lg bg-white flex gap-2", selected && "border-black")}>
         <Rabbit/> <p className="bold">{data.name}</p>
     </div>
 }

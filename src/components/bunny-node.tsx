619c3cb41ab5f8c235d542faf622fdc1ea4ab4fe
Gurvann BRENNE <gurvann.brenne@gmail.com>
2026-04-14 10:47:49 +0200

    :lipstick: improved custom node

--- src/components/bunny-node.tsx
+++ src/components/bunny-node.tsx
@@ -1,6 +1,9 @@
 import {NodeProps} from "reactflow";
 import {BunnyData} from "@/lib/types";
+import {Rabbit} from "lucide-react";
 
 export default function BunnyNode({data}: NodeProps<BunnyData>) {
-    return <div className="border p-2 bg-red-500">{data.name}</div>
+    return <div className="border p-2 rounded-lg bg-white flex gap-2">
+        <Rabbit/> <p className="bold">{data.name}</p>
+    </div>
 }
